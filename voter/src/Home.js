a511397329821cf4e095e67e7fd320c1ede4fcd6
Mayank Gaikwad <8110509+mgdevstack@users.noreply.github.com>
2023-03-02 22:19:13 +0530

    Fix var use

--- voter/src/Home.js
+++ voter/src/Home.js
@@ -36,7 +36,7 @@ class Home extends Component {
   componentDidMount() {
     let r = Math.random().toString(36).substring(7);
     this.setState({ voter_id: r });
-    console.log('EC server endpoint:', ${ec_server_endpoint})
+    console.log('EC server endpoint:', ec_server_endpoint)
     fetch(`http://${ec_server_endpoint}`, {
       method: 'GET',
       headers: {
@@ -50,7 +50,7 @@ class Home extends Component {
       })
       .catch((error) => {
         console.error(
-          'ballot service is not reachable at http://' + ${ec_server_endpoint}
+          'ballot service is not reachable at http://' + ec_server_endpoint
         );
       });
   }
